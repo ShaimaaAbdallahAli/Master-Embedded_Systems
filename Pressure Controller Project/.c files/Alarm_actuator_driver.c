1d4018d8dde21a75a1abf8cfde3e1275b20db3c8
shaimaa <137608852+ShaimaaAbdallahAli@users.noreply.github.com>
2024-12-19 21:02:20 +0200

    Create Alarm_actuator_driver.c

---
+++ Pressure Controller Project/.c files/Alarm_actuator_driver.c
@@ -0,0 +1,41 @@
+/*
+ * Alarm_actuator_driver.c
+ *
+ *      Author: Shaimaa Khattab
+ */
+
+
+#include "Alarm_actuator_driver.h"
+#include "GPIO.h"
+#include "state.h"
+
+void Alarm_monitor_init()
+{
+	//printf("Alarm_monitor_init");
+}
+
+void (*PD_Alarm_actuator_state)();
+
+void startAlarm()
+{
+	PD_Alarm_actuator_state = STATE(AlarmON);
+
+}
+
+void stopAlarm()
+{
+	PD_Alarm_actuator_state = STATE(AlarmOFF);
+}
+
+STATE_define(AlarmON)
+{
+	//State_name
+	Alarm_actuator_state = AlarmON ;
+	Set_Alarm_actuator(0);
+}
+
+STATE_define(AlarmOFF)
+{
+	Alarm_actuator_state = AlarmOFF ;
+		Set_Alarm_actuator(1);
+}
