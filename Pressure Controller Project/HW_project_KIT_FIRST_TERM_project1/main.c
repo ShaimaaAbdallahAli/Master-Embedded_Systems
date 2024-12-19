20dbb75a3b98f5efe24d157ab79d31d03662a629
shaimaa <137608852+ShaimaaAbdallahAli@users.noreply.github.com>
2024-12-19 20:51:38 +0200

    Pressure Controller Project

---
+++ Pressure Controller Project/HW_project_KIT_FIRST_TERM_project1/main.c
@@ -0,0 +1,41 @@
+/*
+ * main.c
+ *      Author: Shaimaa Khattab
+ */
+
+
+#include "stdint.h"
+#include "stdio.h"
+#include "GPIO.h"
+#include "Pressure_sensor_driver.h"
+#include "Main_ALG.h"
+#include "Alarm_monitor.h"
+#include "Alarm_actuator_driver.h"
+#include "Platform_Types.h"
+#include "state.h"
+
+
+int threshold = 20;
+
+void setup()
+{
+	GPIO_INITIALIZATION();
+	Pressure_Sensor_driver_init();
+	PD_Pressure_Sensor_driver_state = STATE(PD_reading);
+	PD_alarm_monitor_state = STATE(PD_alarmOFF) ;
+	PD_Alarm_actuator_state = STATE(AlarmOFF);
+
+}
+int main (){
+	setup();
+
+	while (1)
+	{
+		PD_Pressure_Sensor_driver_state();
+		PD_alarm_monitor_state();
+		PD_Alarm_actuator_state();
+		PD_alarm_monitor_state();
+		PD_Alarm_actuator_state();
+	}
+}
+
